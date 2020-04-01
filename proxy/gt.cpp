419d0c91ec2e2cc90ff336b2304a83460b3acb91
xXAbood <59313717+xXAbood@users.noreply.github.com>
2020-04-01 15:55:10 +0400

    Update version

--- proxy/gt.cpp
+++ proxy/gt.cpp
@@ -4,7 +4,7 @@
 #include "server.h"
 #include "utils.h"
 
-std::string gt::version = "3.021";
+std::string gt::version = "3.32";
 std::string gt::flag = "mv";
 bool gt::resolving_uid2 = false;
 bool gt::connecting = false;
